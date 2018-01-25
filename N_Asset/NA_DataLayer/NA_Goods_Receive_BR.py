bea364000f02b3f143deee8133e7730d5d4bbcf9
nandar <nandar.kusnandar@nufarm.com>
2018-01-25 17:35:21 +0700

    Hari Kemis 2018 januari jam 4

--- N_Asset/NA_DataLayer/NA_Goods_Receive_BR.py
+++ N_Asset/NA_DataLayer/NA_Goods_Receive_BR.py
@@ -20,5 +20,7 @@ class NA_BR_Goods_Receive:
 			colKey = 'sp.supliername'
 		elif columnKey == 'received_by':
 			colKey ==  'ngr.FK_ReceivedBy'
-		return NAGoodsReceive._default_manager.raw("""ngr.IDapp,g.goodsname + ' ' + g.brandname + ' ' IFNULL(g.typeapp,'') as goods , \
-		ngr.datereceived,sp.supliername,ngr.FK_ReceivedBy,emp.Employee_Name AS received_by,FK_P_R_By AS pr_by,ngr.totalPurchased,ngr.totalreceived ORDER BY ngr.IDapp DESC""")
+		return NAGoodsReceive._default_manager.raw("""ngr.IDapp,g.goodsname + ' ' + g.brandname + ' ' IFNULL(g.typeapp,'') as goods , 
+		ngr.datereceived,sp.supliername,ngr.FK_ReceivedBy,emp1.Received_By,FK_P_R_By AS pr_by,emp2.pr_by,ngr.totalPurchased,ngr.totalreceived 
+		FROM n_a_goods_receive AS ngr INNER JOIN n_a_suplier AS sp ON sp.SuplierCode = ngr.FK_Suplier LEFT OUTER JOIN (SELECT IDApp,Employee_Name AS Received_By FROM employee WHERE InActive = 0) 
+	    FROM ORDER BY ngr.IDapp DESC""")
